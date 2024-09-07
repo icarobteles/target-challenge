24677b17ef1d2cf9a04e5a212273e129b3cf4e45
Ícaro Teles <icaro.teles@asaas.com.br>
2024-09-07 15:56:44 -0300

    feat: add application logic to execute questions

--- src/main/java/com/target/Application.java
+++ src/main/java/com/target/Application.java
@@ -1,9 +1,66 @@
 package com.target;
 
-import static com.target.console.printers.PrinterConsole.println;
+import com.target.console.readers.ReaderConsoleScanner;
+import com.target.questions.*;
+
+import java.util.Locale;
+import java.util.Set;
+
+import static com.target.console.printers.PrinterConsole.*;
+import static com.target.console.readers.NumberReaderConsole.readInt;
 
 public class Application {
+
+    @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) {
-        println("Hello World");
+        Locale.setDefault(new Locale("pt", "BR"));
+        Integer question;
+
+        while (true) {
+            question = readInt(Set.of(1, 2, 3, 4, 5, 0), "Digite o número da questão que deseja executar (0 para sair): ");
+
+            switch (question) {
+                case 1 -> questionOne();
+                case 2 -> questionTwo();
+                case 3 -> questionThree();
+                case 4 -> questionFour();
+                case 5 -> questionFive();
+                default -> end();
+            }
+
+            printbl();
+        }
+    }
+
+    private static void questionOne() {
+        clear();
+        QuestionOne.execute();
+    }
+
+    private static void questionTwo() {
+        clear();
+        QuestionTwo.execute();
+    }
+
+    private static void questionThree() {
+        clear();
+        QuestionThree.execute();
+    }
+
+    private static void questionFour() {
+        clear();
+        QuestionFour.execute();
+    }
+
+    private static void questionFive() {
+        clear();
+        QuestionFive.execute();
+    }
+
+    private static void end() {
+        clear();
+        println("Fim da execução");
+        ReaderConsoleScanner.close();
+        System.exit(0);
     }
 }
